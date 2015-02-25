4b1aaa6bde2e419914e392eba7cfce55d5384906
Asav Patel <asavpatel92@gmail.com>
2015-02-24 19:17:06 -0800

    add some more methods for binary tree.

--- Tree/binary_tree.py
+++ Tree/binary_tree.py
@@ -1,6 +1,7 @@
 #===============================================================================
 # implementation for binary tree
 #===============================================================================
+import Queue
 
 class Node:
     
@@ -115,11 +116,75 @@ class BinaryTree:
             self.pre_order_traverse(node.left_child)
             self.pre_order_traverse(node.right_child)
             
+    def pre_order_traverse_iterative(self, node):
+        if node is not None:
+            temp_stack = []
+            temp_stack.append(node)
+            while len(temp_stack) > 0:
+                value = temp_stack.pop()
+                print value
+                if value.right_child: 
+                    #notice here we are pushing right child first into the stack because of LIFO structure of stack. 
+                    #so left child will be took out first. 
+                    temp_stack.append(value.right_child)
+                if value.left_child:
+                    temp_stack.append(value.left_child)
+            return
+            
     def post_order_traverse(self, node):
         if( node is not None):
             self.post_order_traverse(node.left_child)
             self.post_order_traverse(node.right_child)
             print "%s" %node
+
+    #===========================================================================
+    #     for eg. this is the tree:
+    #                 10
+    #                /   \  
+    #               5    30
+    #              / \   /  \ 
+    #             4  8  28  42
+    #     this is the formula for calculating the height of the tree : height(node) = max(height(node.Left_child), height(node.Right_child)) + 1
+    #     here's how below method works : 
+    #     height(10) = max(height(5), height(30)) + 1
+    # 
+    #     height(30) = max(height(28), height(42)) + 1
+    #     height(42) = 1 (no children)
+    #     height(28) = 1 (no children)
+    #     
+    #     height(5) =  max(height(4), height(8)) + 1
+    #     height(4) = 1 (no children)
+    #     height(8) = 1 (no children)
+    #     so now,
+    #     height(5)  = max(1, 1) + 1
+    #     height(30) = max(1, 1) + 1
+    #     height(10) = max(2, 2) + 1
+    #     height(10) = 3 --> which is the height of the tree          
+    #===========================================================================
+    def get_height(self, node):
+        if node is None:
+            return 0
+        else:
+            return max(self.get_height(node.left_child), self.get_height(node.right_child)) + 1
+        
+    
+    #this is similar to do a BFS on a tree it is also called level order traversal.   
+    def print_binary_tree(self, node):
+        queue = Queue.Queue()
+        queue.put(node)
+        queue.put("newline")
+        while not queue.empty():
+            node = queue.get()
+            if node is "newline":
+                print "\n"
+                if not queue.empty():
+                    queue.put("newline")
+            else:
+                print node , "    " ,
+                if node.left_child is not None:
+                    queue.put(node.left_child)
+                if node.right_child is not None:
+                    queue.put(node.right_child)
 
 def main():
     new_binary_tree = BinaryTree()
@@ -129,14 +194,23 @@ def main():
     new_binary_tree.add_node(30, "Secretary");
     new_binary_tree.add_node(75, "Sales Manager");
     new_binary_tree.add_node(85, "Salesman 1");
+    
     print "In Order Traversal:"
     new_binary_tree.in_order_traverse(new_binary_tree.root)
+    
     print "\nPre Order Traversal:"
     new_binary_tree.pre_order_traverse(new_binary_tree.root)
+    
     print "\nPost Order Traversal:"
     new_binary_tree.post_order_traverse(new_binary_tree.root)
+    
     print "\nSearch for key: 25"
     new_binary_tree.find_node(25)
+    
+    print "Height of the tree:" , new_binary_tree.get_height(new_binary_tree.root)
+    
+    print "Printing the tree -->"
+    new_binary_tree.print_binary_tree(new_binary_tree.root)
 
 if __name__ == '__main__':
     main()
